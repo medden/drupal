b2a93c1954e321566bd0ec801a1dada51327ae1b
medden <si@medden.co.uk>
2012-02-01 14:53:54 +0000

    added less

--- sites/all/modules/live_css/less-display.js
+++ sites/all/modules/live_css/less-display.js
@@ -1,11 +1,9 @@
 (function($) {
   $('link').each(function() {
-    if ($(this).attr('href').match(/\.less/i) != null) {
+    if ($(this).attr('href').match(/\.less$/i) != null) {
       $(this).attr('rel', 'stylesheet/less');
       var href = $(this).attr('href');
-      if (href.substring(0, 1) != '/')
-        href = '/' + href;
-      $(this).attr('href', href);
+      $(this).attr('href', '/' + href);
     }
   });
 })(jQuery);
